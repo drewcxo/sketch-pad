07470ade9084b91a094fefe47b831c00a3a7de42
drewcxo <cox0727@gmail.com>
2024-04-26 17:09:16 -0500

    sketch pad complete

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ function populateBoard(size) {
   let squares = board.querySelectorAll('div');
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
-    let square = document.createElement("div");
+    let square = document.createElement('div');
     square.addEventListener('mouseover', colorSquare);
     square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
@@ -19,8 +19,8 @@ function populateBoard(size) {
 
 populateBoard(16);
 
-function changeSize (input) {
-if (input >= 2 && input <= 100) {
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
   document.querySelector('.error').style.display = 'none';
   populateBoard(input);
 } else {
@@ -28,18 +28,33 @@ if (input >= 2 && input <= 100) {
   }
 }
 
+function colorSquare() {
+  if (click) {
+    if (color === 'random') {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
 
+function changeColor(choice) {
+  color = choice;
+}
 
+function resetBoard(){
+  let board = document.querySelector('.board');
+  let squares = board.querySelectorAll('div');
+  squares.forEach((div) => (div.style.backgroundColor = 'white'));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+document.querySelector('body').addEventListener('click', (e) => {
+  if (e.target.tagname != 'BUTTON') {
+    click = !click;
+    if (click) {
+      document.querySelector('.mode').textContent = 'Mode Coloring';
+    } else {
+      document.querySelector('.mode').textContent = 'Mode: Not coloring';
+    }
+  }
+});
